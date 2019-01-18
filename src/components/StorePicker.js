f8d5691847946fa86576a6e265bf643f11f279e0
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-01-18 16:55:00 +0000

    Include helper functions and tie random names to store name input box

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react';//only want render from the react-dom lib
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form action="" className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" name="" id="" required placeholder="Store name" />
+        <input type="text" name="" id="" required placeholder="Store name" defaultValue={getFunName()} />
         <button type="submit">Visit store</button>
       </form>
     )
