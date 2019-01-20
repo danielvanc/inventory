5b688d0f7b7cf5daa7a0b6c659a58870749ed660
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-01-20 23:12:29 +0000

    Add routing for main store picker using history.push

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,21 @@ import React from 'react';//only want render from the react-dom lib
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  // constructor() {
+    // super();
+    // this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore = (event)  => {
+    event.preventDefault();
+    const storeName = this.myInput.current.defaultValue;
+    this.props.history.push(`/store/${storeName}`) 
+  }
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" name="" id="" required placeholder="Store name" defaultValue={getFunName()} />
+        <input ref={this.myInput} type="text" name="" id="" required placeholder="Store name" defaultValue={getFunName()} />
         <button type="submit">Visit store</button>
       </form>
     )
