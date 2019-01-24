12beb731eb0025a1a1f3335f3a6d77721602e5d3
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-01-24 21:09:39 +0000

    Add ordered fish to order in state

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,15 @@ import Header from './Header'
 import Inventory from './Inventory'
 import Order from './Order'
 import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 class App extends React.Component {
-  
   state = {
     fishes: {},
     order: {}
   }
   addFish = fish => {
-    // take a copy of the existing state
     const fishes = {...this.state.fishes}
     fishes[`fish${Date.now()}`] = fish;
-    // state: ...
     this.setState({
       fishes
     })
@@ -23,14 +21,26 @@ class App extends React.Component {
       fishes: sampleFishes
     })
   }
+  addToOrder = key => {
+    const order = {...this.state.order }
+    order[key] = order[key] + 1 || 1
+    this.setState({
+      order
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh seafood market" /> 
+          <ul className="fishes">
+            {
+              Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+            }
+          </ul>
         </div>
         <Order /> 
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/> 
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} /> 
       </div>
     )
   }
